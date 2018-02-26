e6cf5cbc0ee0e22b48fff3d15915398b0cf16618
David Lewis and John Rykken <david@>
2018-02-26 12:11:47 -0800

    Fail first spec, work on spec_helper

--- spec/region_spec.rb
+++ spec/region_spec.rb
@@ -1,1 +1,13 @@
 require("spec_helper")
+
+describe(Region) do
+
+  describe("#trails") do
+    it("tells which trails are in it") do
+      test_region = Region.create({:name => "bend area"})
+      test_trail1 = Trail.create({:name => "smith rock loop", :region_id => test_region.id})
+      test_trail2 = Trail.create({:name => "jefferson trail", :region_id => test_region.id})
+      expect(test_region.trails()).to(eq([test_trail1, test_trail2]))
+    end
+  end
+end
